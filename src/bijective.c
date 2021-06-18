6c343073f6162cd755df534cacddd5d366e70222
Kris Nóva <kris@nivenly.com>
2021-06-18 10:46:38 -0700

    adding comments to bijective code
    Signed-off-by: Kris Nóva <kris@nivenly.com>

--- src/bijective.c
+++ src/bijective.c
@@ -27,30 +27,39 @@
 #include <stdlib.h>
 
 
-// ----------------------------------------------------------+
-// Each SHA-256 Digest is 32 Bytes ling.                     |
-// 32 Bytes = (32 * 8 bits) = 256 bits = {0,255}             |
-// 32 Bytes = 8 Bytes * 4 Segments                           |
-// ----------------------------------------------------------+
-//
-//
-// [ 8 Bytes ] 1 Segment
-// +---------------------------------------------------------+
-// | [  0]  [  1]  [  2]  [  3]  [  4]  [  5]  [  6]  [  7]  |
-// | [255]  [255]  [255]  [255]  [255]  [255]  [255]  [255]  |
-// | [16^2] [16^2] [16^2] [16^2] [16^2] [16^2] [16^2] [16^2] |
-// | [0F]   [0F]   [0F]   [0F]   [0F]   [0F]   [0F]   [0F]   |
-// +---------------------------------------------------------+
-//
-//
-// [8 Bytes] 4 Pairs (1 Pair = 0->130,560)
-// +---------------------------------------------------------+
-// | [  0]  [  1]  [  2]  [  3]  [  4]  [  5]  [  6]  [  7]  |
-// | [0,130560  ]  [0,130560  ]  [0,130560  ]  [0,130560  ]  |
-// +---------------------------------------------------------+
-//
-// dname_bijection is a deterministic bijective function for
-// a 32 byte sha256 digest.
+/**
+ * dname_bijection()
+ *
+ * Take a 32 byte digest from sha-256 and create an 8 byte
+ * bijective name from the name list.
+ *
+ * ----------------------------------------------------------+
+ * Each SHA-256 Digest is 32 Bytes ling.                     |
+ * 32 Bytes = (32 * 8 bits) = 256 bits = {0,255}             |
+ * 32 Bytes = 8 Bytes * 4 Segments                           |
+ * ----------------------------------------------------------+
+ *
+ *
+ * [ 8 Bytes ] 1 Segment
+ * +---------------------------------------------------------+
+ * | [  0]  [  1]  [  2]  [  3]  [  4]  [  5]  [  6]  [  7]  |
+ * | [255]  [255]  [255]  [255]  [255]  [255]  [255]  [255]  |
+ * | [16^2] [16^2] [16^2] [16^2] [16^2] [16^2] [16^2] [16^2] |
+ * | [0F]   [0F]   [0F]   [0F]   [0F]   [0F]   [0F]   [0F]   |
+ * +---------------------------------------------------------+
+ *
+ *
+ * [8 Bytes] 4 Pairs (1 Pair = 0->130,560)
+ * +---------------------------------------------------------+
+ * | [  0]  [  1]  [  2]  [  3]  [  4]  [  5]  [  6]  [  7]  |
+ * | [0,130560  ]  [0,130560  ]  [0,130560  ]  [0,130560  ]  |
+ * +---------------------------------------------------------+
+ *
+ * dname_bijection is a deterministic bijective function for
+ * a 32 byte sha256 digest.
+ *
+ * @param digest
+ */
 void dname_bijection(struct dname_digest *digest) {
 
     // [8 Byte] truncated bijective name
@@ -70,6 +79,18 @@ void dname_bijection(struct dname_digest *digest) {
 
 // dname_pair implements the Cantor pairing function for
 // deterministically pairing integers together.
+
+/**
+ * dname_pair()
+ *
+ * Use math (The Cantor Pairing Function) to deterministically pair integers together.
+ *
+ * We know we are paring bytes, so we know this value will never exceed 130,560.
+ *
+ * @param i1
+ * @param i2
+ * @return int
+ */
 int dname_pair(int i1, int i2) {
     return (i1+i2) * (i1+i2+1) / 2 + i2;
 }
